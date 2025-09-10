7546493e88ba32450e820f8edd29065d4aafffc7
Hydronic1219 <hyunwoong1219@gmail.com>
2025-09-10 14:14:59 +0900

    first

---
+++ ap.h
@@ -0,0 +1,20 @@
+#ifndef __AP_H__
+#define __AP_H__
+
+
+#include "../driver/led.h"
+#include "../driver/button.h"
+#include "../driver/led8.h"
+#include "../driver/main_motor.h"
+#include "../driver/sub_motor.h"
+#include "../driver/myled.h"
+
+
+
+
+void apInit();
+void apMain();
+
+
+
+#endif /* __AP_H__ */
---
+++ def.h
@@ -0,0 +1,14 @@
+#ifndef __DEF_H__
+#define __DEF_H__
+
+#include <stdint.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+#include <util/delay.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+#endif /* __DEF_H__ */
